51489def60b206b34ae8156f92f23d56fde6ecaf
Aniket <aniketdhokane2071@gmail.com>
2022-04-14 00:30:57 +0530

    Route Files

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT|| 5000
 
 const app = express()
 
-app.get('/api/goals',(req,res) => {
-    res.status(200).json({message:"get goals"})
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port)
 
